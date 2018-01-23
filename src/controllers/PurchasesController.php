5e51d63497753ec5848b59df0f5e3314104cdd2e
njerucyrus <njerucyrus5@gmail.com>
2018-01-23 14:09:16 +0300

    added  filter by date function

--- src/controllers/PurchasesController.php
+++ src/controllers/PurchasesController.php
@@ -13,11 +13,12 @@ use src\db\DB;
 class PurchasesController implements CrudInterface
 {
     protected $conn;
+    protected $db;
 
     public function __construct()
     {
-        $db = new DB();
-        $this->conn = $db->connect();
+        $this->db = new DB();
+        $this->conn = $this->db->connect();
     }
 
 
@@ -42,10 +43,12 @@ class PurchasesController implements CrudInterface
             $stmt->bindParam(":amount_paid", $data['amount_paid']);
 
             if ($stmt->execute()) {
+                $this->db->closeConnection();
                 return [
                     "status_code" => 201,
                     "message" => "Transaction Recorded Successfully"
                 ];
+
             } else {
                 return [
                     "status_code" => 500,
@@ -82,6 +85,7 @@ class PurchasesController implements CrudInterface
             $stmt->bindParam(":amount_paid", $data['amount_paid']);
 
             if ($stmt->execute()) {
+                $this->db->closeConnection();
                 return [
                     "status_code" => 201,
                     "message" => "Transaction Updated Successfully"
@@ -109,6 +113,8 @@ class PurchasesController implements CrudInterface
                 ->prepare("DELETE FROM purchases WHERE id=:id");
 
             if ($stmt->execute()) {
+                (new self)->db->closeConnection();
+
                 return [
                     "status_code" => 201,
                     "message" => "Transaction Record deleted Successfully"
@@ -136,6 +142,7 @@ class PurchasesController implements CrudInterface
                 ->prepare("SELECT * FROM purchases WHERE id=:id LIMIT 1");
 
             if ($stmt->execute() && $stmt->rowCount() > 0) {
+                (new self)->db->closeConnection();
                 return [
                     "status_code" => 200,
                     "data" => $stmt->fetch(\PDO::FETCH_ASSOC)
@@ -163,6 +170,38 @@ class PurchasesController implements CrudInterface
                 ->prepare("SELECT * FROM purchases WHERE  1");
 
             if ($stmt->execute() && $stmt->rowCount() > 0) {
+                (new self)->db->closeConnection();
+                return [
+                    "status_code" => 200,
+                    "data" => $stmt->fetchAll(\PDO::FETCH_ASSOC)
+                ];
+            } else {
+                return [
+                    "status_code" => 500,
+                    "message" => "Error occurred => {$stmt->errorInfo()}"
+                ];
+            }
+
+
+        } catch (\PDOException $e) {
+            return [
+                "status_code" => 500,
+                "message" => "Error occurred => {$e->getMessage()}"
+            ];
+        }
+    }
+
+    public static function getDayRecords($date)
+    {
+        try {
+            $stmt = (new self)->conn
+                ->prepare("SELECT * FROM purchases WHERE  date_paid=:date_paid");
+            $stmt->bindParam(":date_paid", $date);
+
+            if ($stmt->execute() && $stmt->rowCount() > 0) {
+
+                (new self)->db->closeConnection();
+
                 return [
                     "status_code" => 200,
                     "data" => $stmt->fetchAll(\PDO::FETCH_ASSOC)
